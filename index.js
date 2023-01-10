41f3e8258e2a6a22fae8a40d4523de721fe0f57e
Jaclyn Williams <jaclyn.williams143@gmail.com>
2023-01-09 20:10:06 -0500

    commit tree fd7efec84b0fce1ec3ca87d342ef82dd0b8963ad

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// First Attempt
+// document.addEventListener("DOMContentLoaded", (e) => {"JavaScript is so cool. It lets me add text to my page programmatically."});
+
+// document.getElementById('text').innerHTML = "This is really cool!";
+
+// Second Attempt 
+// document.addEventListener("DOMContentLoaded", () => {document.getElementById('text').innerHTML = "This is really cool!"};
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('text').innerHTML = "This is really cool!"
+})
